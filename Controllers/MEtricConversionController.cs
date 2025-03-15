dd1e56b3fe5de8d2d3f5990b6e966f553fd9c4ae
Faik Hakan Bilgen <faikb@microsoft.com>
2025-03-15 09:58:23 +0300

    Added new controller

---
+++ Controllers/MEtricConversionController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MEtricConversionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
